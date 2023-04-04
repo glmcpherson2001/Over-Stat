6b1d56c1eff3608e5e6a7cb5f9721601857e8090
DominicThoele <127760223+DominicThoele@users.noreply.github.com>
2023-04-04 13:11:12 -0500

    yourdatadone/excepttimer

--- views/script.js
+++ views/script.js
@@ -14,15 +14,30 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let slideContainer = document.querySelector('.slideshow-container');
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "flex";  
+  var slideType = document.activeElement.id;
+  if (slideIndex === 1){
+    slideContainer.style.backgroundImage = "url(slideshow_imgs/tank_slide.png)"
+    }
+  else if(slideIndex === 2){
+    slideContainer.style.backgroundImage = "url(slideshow_imgs/damage_slide.jpg)"
+  }
+  else if (slideIndex === 3){
+    slideContainer.style.backgroundImage = "url(slideshow_imgs/support_slide.png)"
+  }
+  slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 }
+
+setInterval(function() {
+  plusSlides(1);
+}, 10000);
