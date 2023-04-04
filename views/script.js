c90e161045a8a1c67e56d122b0753591b85b100f
glmcpherson2001 <glmcpherson2001@gmail.com>
2023-04-04 13:15:59 -0500

    Fixed Auto Scrolling

--- views/script.js
+++ views/script.js
@@ -1,5 +1,3 @@
-
-// Slideshow Code
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -16,28 +14,27 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let slideContainer = document.querySelector('.slideshow-container');
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  var slideType = document.activeElement.id;
   if (slideIndex === 1){
-    slideContainer.style.backgroundImage = "url(slideshow_imgs/tank_slide.png)"
-    }
-  else if(slideIndex === 2){
-    slideContainer.style.backgroundImage = "url(slideshow_imgs/damage_slide.jpg)"
+    slideContainer.style.backgroundImage = "url('slideshow_imgs/tank_slide.png')"
   }
-  else if (slideIndex === 3){
-    slideContainer.style.backgroundImage = "url(slideshow_imgs/support_slide.png)"
+  else if(slideIndex  === 2){
+    slideContainer.style.backgroundImage = "url('slideshow_imgs/damage_slide.jpg')"
+  }
+  else if (slideIndex  === 3){
+    slideContainer.style.backgroundImage = "url('slideshow_imgs/support_slide.png')"
   }
   slides[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].className += " active";
 }
 
-setInterval(function() {
-  plusSlides(1);
+let slideInterval = setInterval(function() {
+  slideIndex++;
+  showSlides(slideIndex);
 }, 10000);
